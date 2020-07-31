543c66cda830f24c7700a9eed326b6a80bdd36a2
Saurabh Narula <saurabh.narula@electrifai.net>
2020-07-31 17:53:18 -0400

    delete redundant appimage shell script.

--- appimage/build-gcr.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-version=1.0
-image=procurementai
-
-docker build -t $image:$version .
-docker tag $image:$version gcr.io/electrifai-public/$image:$version
-docker push gcr.io/electrifai-public/$image:$version
